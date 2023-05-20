7d733ff989e45c35edf277fc82428b5b58a23382
Tarun Singh <tarunsinghofficial236@gmail.com>
2023-05-20 11:15:33 +0530

    Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+    RWOnly obj = new RWOnly();
+    // java: name has private access in com.driver.RWOnly
+//    obj.name = "Hello";
+//    System.out.println(obj.name);
+      obj.setName("Hello");
+      System.out.println(obj.getName());
+    }
 }
